bce4e245f0ba43490feba4459ea25aa8ea9c27e8
faun3 <antonioculcea@gmail.com>
2023-08-09 13:03:25 +0300

    make posts GET return posts collection as json

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import connect from "@/utils/db";
+import Post, { PostInterface } from "@/models/Post";
 
 export const GET = async (req: Request) => {
-  return new NextResponse("cool", { status: 200 });
+  try {
+    await connect();
+    const posts: PostInterface[] = await Post.find();
+    return NextResponse.json(posts, { status: 200 });
+  } catch (err) {
+    return new NextResponse("db error", { status: 500 });
+  }
 };
