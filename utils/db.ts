247bc5be18c6d1057465da37fa05fb073241c25e
faun3 <antonioculcea@gmail.com>
2023-08-09 11:20:49 +0300

    replace v7 of mongoose with 6.11.5

---
+++ utils/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    if (!process.env.MONGO) {
+      console.log("MONGO key is not present  in local .env file");
+      throw new Error("mongo key not present in .env");
+    } else {
+      await mongoose.connect(process.env.MONGO);
+    }
+  } catch (error) {
+    throw new Error("connection failed");
+  }
+};
+
+export default connect;
