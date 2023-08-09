2c4678fd513bb8ef062bc0c83d760a7ea06920f1
faun3 <antonioculcea@gmail.com>
2023-08-09 23:18:50 +0300

    refactor slider

--- components/DarkModeToggle/DarkModeToggle.tsx
+++ components/DarkModeToggle/DarkModeToggle.tsx
@@ -7,16 +7,20 @@ const DarkModeToggle = () => {
   const { dispatch, payload } = useContext(ThemeContext);
   return (
     <div
-      className="flex rounded-full px-[2px] border-emerald-500 border-2 relative cursor-pointer gap-1"
+      className={`flex rounded-full px-[2px] border-emerald-500 border-2 relative cursor-pointer gap-2 h-[1.5rem] w-[3rem] ${
+        payload === "dark" ? "bg-slate-200" : "bg-slate-700"
+      }`}
       onClick={() => {
         dispatch();
-      }}>
-      <div>🌙</div>
-      <div>☀️</div>
+      }}
+    >
+      {/* <div>🌙</div> */}
+      {/* <div>☀️</div> */}
       <div
-        className={`bg-emerald-500 rounded-full h-[20px] w-[20px] absolute top-[2px] ${
-          payload === "dark" ? "" : "translate-x-[calc(100%+8px)]"
-        } transition-all`}></div>
+        className={`bg-emerald-500 rounded-full h-[1.4rem] w-[1.4rem] absolute left-[-1px] top-[-1px] ${
+          payload === "dark" ? "" : "translate-x-[calc(100%+1px)]"
+        } transition-all`}
+      ></div>
     </div>
   );
 };
