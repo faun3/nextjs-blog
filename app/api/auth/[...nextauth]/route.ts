d399ffa0c8e4e7f0bbebb1554d4add9c00b03ce8
faun3 <antonioculcea@gmail.com>
2023-08-10 23:15:09 +0300

    bad borked not working auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,30 @@
 //TODO - implement google auth (this needs a google cloud account)
 // timestamp -- 2:18:02
+import { connect } from "http2";
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/github'
+import { CredentialsProvider } from "next-auth/providers/credentials";
 
-export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID || 'nothing',
-            clientSecret: process.env.GOOGLE_SECRET || 'nothing',
-        })
-    ]
-})
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+    id:'Credentials',
+    name: 'Credentials',
+    credentials: {
+        username: {label: 'Username', type: 'text'},
+        password: {label: 'Password', type: 'password'}
+    }
+    async authorize(credentials){
+        await connect();
+        try {
+
+        } catch (err){
+            let message = 'Throwing non errors? Wow.';
+            if (err instanceof Error) message = err.message;
+            throw new Error(message)
+        }
+    }
+  })
+  ],
+});
+
+export {handler as GET, handler as POST};
