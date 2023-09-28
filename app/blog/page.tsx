fa6e215a35980709b9af5ec34843aa68983f2c16
faun3 <antonioculcea@gmail.com>
2023-09-28 10:09:28 +0300

    feat: fixed some bad stuff from the past

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import { PostInterface } from "@/models/Post";
+import url from "@/utils/url";
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
+  const res = await fetch(`${url}/api/posts`, {
     next: { revalidate: 10 },
   });
 
@@ -25,18 +26,19 @@ const Blog = async () => {
     <div className="my-8">
       {data.map((post) => {
         return (
-          <Link href={`blog/${post._id}`} key={post._id}>
+          <Link
+            href={`blog/${post._id}`}
+            key={post._id}>
             <div className="flex flex-row items-center gap-5 my-8">
               <div className="relative w-[400px] h-[250px] min-w-[400px] overflow-hidden rounded-lg max-w-[400px]">
                 <Image
                   src={post.img}
                   alt="design studio"
                   fill={true}
-                  className="object-cover"
-                ></Image>
+                  className="object-cover"></Image>
               </div>
               <div>
-                <h1 className="font-bold text-emerald-200 dark:text-emerald-800 text-2xl mb-2">
+                <h1 className="mb-2 text-2xl font-bold text-emerald-200 dark:text-emerald-800">
                   {post.title}
                 </h1>
                 <p>{post.desc}</p>
---
+++ utils/url.ts
@@ -0,0 +1,2 @@
+const url = "https://dremt.vercel.com";
+export default url;
